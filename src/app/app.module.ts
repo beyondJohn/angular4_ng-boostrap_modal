1d10cbc8c79542b6801a5a1ca381940c0d774766
JP <jp@beyondlogical.com>
2017-11-29 00:54:11 -0500

    modal installed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
 
+import { ModalContentComponent, NgbdModalComponent } from './modal-component/modal-component.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ModalContentComponent,
+    NgbdModalComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
+  providers: [NgbActiveModal],
+  entryComponents: [ModalContentComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/modal-component/modal-component.component.ts
@@ -0,0 +1,43 @@
+import { Component, Input} from '@angular/core';
+
+import { NgbModal, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-modal-component',
+
+  template: `
+  <div class="modal-header">
+    <h4 class="modal-title">Hi there!</h4>
+    <button type="button" class="close" aria-label="Close" (click)="activeModal.dismiss('Cross click')">
+      <span aria-hidden="true">&times;</span>
+    </button>
+  </div>
+  <div class="modal-body">
+    <p>Hello, {{name}}!</p>
+  </div>
+  <div class="modal-footer">
+    <button type="button" class="btn btn-outline-dark" (click)="activeModal.close('Close click')">Close</button>
+  </div>
+`,
+  styleUrls: ['./modal-component.component.css']
+})
+export class ModalContentComponent {
+
+  @Input() name;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+}
+
+@Component({
+  selector: 'ngbd-modal-component',
+  templateUrl: './modal-component.component.html'
+})
+export class NgbdModalComponent {
+  constructor(private modalService: NgbModal) {}
+
+  open() {
+    const modalRef = this.modalService.open(ModalContentComponent);
+    modalRef.componentInstance.name = 'World';
+  }
+}
